d376a75964012023380196f5630822bacac8c2f1
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2022-07-06 16:00:37 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 def square_matrix_simple(matrix=[]):
     if not matrix:
-        return none
-
-    return list(list(map(square, n)) for n in matrix)
+        return None
+    
+    return (list(map(square, n)) for n in matrix)
+ 
