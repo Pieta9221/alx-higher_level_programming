23d86c0600cfdbc3d422a6d5080ad61d6de8e040
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2022-10-03 16:22:44 +0100

    Create 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (x) {
+  if (x > 1) {
+    return x * factorial(x - 1);
+  }
+  if (x < 0) {
+    return undefined;
+  }
+  return 1;
+}
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
